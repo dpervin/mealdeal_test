9e17ba14e6caff9dccc189fea6d7fa8f57a5e62e
dpervin <79943177+dpervin@users.noreply.github.com>
2023-04-02 08:40:36 -0400

    Add files via upload

---
+++ topheaderformat.js
@@ -0,0 +1,5 @@
+<header class="header-container">
+  <img src="mealdeal_logo.png" alt="MealDeal logo" class="logo">
+  <h1>Welcome to MealDeal</h1>
+  <p>MealDeal is your one stop shop for finding the best local deals and groceries and recommending great recipe's based on those deals!</p>
+</header>
